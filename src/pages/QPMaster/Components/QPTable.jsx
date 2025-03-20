d240bbef1edd8d9267a4b9abaa8c25e8111644a8
cuplprojects <shivom.chandrakala@gmail.com>
2025-03-20 13:07:28 +0530

    added conditionally rendering of columns

--- src/pages/QPMaster/Components/QPTable.jsx
+++ src/pages/QPMaster/Components/QPTable.jsx
@@ -11,6 +11,7 @@ import { useMemo } from "react";
 const QPTable = ({ filters, qpData, setShowTable }) => {
   const themeState = useStore(themeStore);
   const cssClasses = useMemo(() => themeState.getCssClasses(), [themeState]);
+  console.log("Filters passed ->",filters)
   const [
     customDark,
     customMid,
@@ -26,80 +27,40 @@ const QPTable = ({ filters, qpData, setShowTable }) => {
     setShowTable(false);
   };
 
-  const columnDefs = [
-    {
-      headerName: "QP Master ID",
-      field: "qpMasterId",
-      sortable: true,
-      filter: true,
-    },
-    {
-      headerName: "Group Name",
-      field: "groupName",
-      sortable: true,
-      filter: true,
-    },
-    {
-      headerName: "Type",
-      field: "type",
-      sortable: true,
-      filter: true,
-    },
-    {
-      headerName: "NEP Code",
-      field: "nepCode",
-      sortable: true,
-      filter: true,
-    },
-    {
-      headerName: "Private Code",
-      field: "privateCode",
-      sortable: true,
-      filter: true,
-    },
-    {
-      headerName: "Subject Name",
-      field: "subjectName",
-      sortable: true,
-      filter: true,
-    },
-    {
-      headerName: "Paper Number",
-      field: "paperNumber",
-      sortable: true,
-      filter: true,
-    },
-    {
-      headerName: "Paper Title",
-      field: "paperTitle",
-      sortable: true,
-      filter: true,
-    },
-    {
-      headerName: "Max Marks",
-      field: "maxMarks",
-      sortable: true,
-      filter: true,
-    },
-    {
-      headerName: "Duration",
-      field: "duration",
-      sortable: true,
-      filter: true,
-    },
-    {
-      headerName: "Course Name",
-      field: "courseName",
-      sortable: true,
-      filter: true,
-    },
-    {
-      headerName: "Exam Type Name",
-      field: "examTypeName",
-      sortable: true,
-      filter: true,
-    },
-  ];
+// Define all possible columns
+const allColumnDefs = [
+  { headerName: "QP Master ID", field: "qpMasterId", sortable: true, filter: true },
+  { headerName: "Group Name", field: "groupName", sortable: true, filter: true },
+  { headerName: "Type", field: "type", sortable: true, filter: true },
+  { headerName: "NEP Code", field: "nepCode", sortable: true, filter: true },
+  { headerName: "Private Code", field: "privateCode", sortable: true, filter: true },
+  { headerName: "Subject Name", field: "subjectName", sortable: true, filter: true },
+  { headerName: "Paper Number", field: "paperNumber", sortable: true, filter: true },
+  { headerName: "Paper Title", field: "paperTitle", sortable: true, filter: true },
+  { headerName: "Max Marks", field: "maxMarks", sortable: true, filter: true },
+  { headerName: "Duration", field: "duration", sortable: true, filter: true },
+  { headerName: "Course Name", field: "courseName", sortable: true, filter: true },
+  { headerName: "Exam Type Name", field: "examTypeName", sortable: true, filter: true },
+];
+
+// Conditionally include columns based on applied filters
+const columnDefs = useMemo(() => {
+  return allColumnDefs.filter(col => {
+    switch (col.field) {
+      case "groupName":
+        return !filters.groupName;
+      case "type":
+        return !filters.selectedTypeName;
+      case "courseName":
+        return !filters.selectedCourseName;
+      case "examTypeName":
+        return !filters.selectedExamTypeName;
+      default:
+        return true;
+    }
+  });
+}, [filters]);
+
 
   return (
     <div className="w-100">
