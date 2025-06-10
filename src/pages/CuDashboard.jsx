4651038d4a2df7253842959c6e316f9bef98e3e8
Akshaya Mishra <shivom.chandrakala@gmail.com>
2025-06-10 12:36:18 +0530

    starred project laste entered project

--- src/pages/CuDashboard.jsx
+++ src/pages/CuDashboard.jsx
@@ -24,7 +24,6 @@ import styled from "styled-components";
 import { getAllProjectCompletionPercentages } from "../CustomHooks/ApiServices/transacationService";
 import { useTranslation } from "react-i18next";
 
-
 const AnimatedDropdownMenu = styled(Dropdown.Menu)`
   &.dropdown-enter {
     opacity: 0;
@@ -80,11 +79,12 @@ const CuDashboard = () => {
     return savedState
       ? JSON.parse(savedState)
       : {
+        // lineChart: true,
+        // pieChart: true,
         agGrid: true,
         barChart: true,
       };
   });
-
   const [visiblecardsIcon] = useState({
     lineChart: LineChartIcon,
     pieChart: PieChartIcon,
@@ -101,7 +101,6 @@ const CuDashboard = () => {
   const pageSize = 5; // Number of projects per page
   const [hasMore, setHasMore] = useState(true);
 
-  // funtion to handle the disabled projects
   const hasDisable = (projectid) => {
     const hasQuantitySheet = hasquantitySheet.find(
       (item) => item.projectId === projectid
