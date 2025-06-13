2d99bb0cf43db883c5166041d8fa0e30e5c0257c
cuplprojects <shivom.chandrakala@gmail.com>
2025-06-13 14:56:13 +0530

    added reset button

--- src/user/UserSettings/UserSettings.jsx
+++ src/user/UserSettings/UserSettings.jsx
@@ -1,6 +1,6 @@
-import React, { useEffect, useRef, useState } from 'react';
+import { useEffect, useState } from 'react';
 import {
-  Container, Nav, Row,
+  Container, Row,
   Col,
 } from 'react-bootstrap';
 import CuDashboardSettings from './Components/CuDashboardSettings';
@@ -10,18 +10,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './UserSettings.css';
 import { useTranslation } from "react-i18next";
 import SecuritySettings from './Components/SecuritySettings/SecuritySettings';
-import { Dropdown } from 'react-bootstrap';
 import themeStore from '../../store/themeStore';
 import { useStore } from 'zustand';
 import { IoSave } from "react-icons/io5";
 import { IoSettingsSharp } from "react-icons/io5";
+import { LuTimerReset } from "react-icons/lu";
 
 const UserSettings = () => {
   const { t } = useTranslation();
-  const dashboardRef = useRef(null);
-  const generalRef = useRef(null);
-  const processRef = useRef(null);
-  const securityRef = useRef(null);
   const { getCssClasses } = useStore(themeStore);
   const [
     customDark,
@@ -33,39 +29,37 @@ const UserSettings = () => {
     customLightBorder,
     customDarkBorder
   ] = getCssClasses();
-  const [isSticky, setIsSticky] = useState(false);
-
-  useEffect(() => {
-    const handleScroll = () => {
-      const scrollThreshold = 100;
-      if (window.scrollY > scrollThreshold) {
-        setIsSticky(true);
-      } else {
-        setIsSticky(false);
-      }
-    };
-    window.addEventListener('scroll', handleScroll);
-    return () => window.removeEventListener('scroll', handleScroll);
-  }, []);
-  const scrollToSection = (ref) => {
-    ref.current.scrollIntoView({ behavior: 'smooth' });
-  };
 
   return (
-    <div className={` ${customLight} user-settings-container rounded-4 shadow-lg`}>
-      <Container className="py-4">
+    <div className={`${customLight} user-settings-container rounded-4 shadow-lg`}>
+      <Container className="py-2">
         <div className="row">
-          <div className="col-12">
-            <div className="d-flex align-items-center justify-content-center mb-4">
-              <h2 className={`${customDarkText} mb-0 text-center d-flex align-items-center justify-content-center`}>
-                <span>
-                  <IoSettingsSharp className='me-2 settings-icon-rotate ' />
-                </span>
-                <span>
-                  {t('userSettings')}
-                </span>
-              </h2>
-            </div>
+          <div className="">
+            <Row className='d-flex align-items-center'>
+              <Col lg={4} md={2}></Col>
+              <Col lg={4} md={8}>
+                <div className="d-flex align-items-center justify-content-center mb-4">
+                  <h2 className={`${customDarkText} mb-0 text-center d-flex align-items-center justify-content-center`}>
+                    <span>
+                      <IoSettingsSharp className='me-2 settings-icon-rotate ' />
+                    </span>
+                    <span>
+                      {t('userSettings')}
+                    </span>
+                  </h2>
+                </div>
+              </Col>
+              <Col lg={4} md={2} className='d-flex justify-content-end'>
+                <button
+                  className={`btn btn-outline-danger  me-2 d-flex align-items-center gap-1`}
+                  title={t('resetAll')}
+                >
+                  <LuTimerReset />
+                  {t('reset')}
+                </button>
+              </Col>
+            </Row>
+
 
             {/* General & Dashbaord Settings */}
             <Row className='mt-5'>
@@ -83,9 +77,7 @@ const UserSettings = () => {
                   <CuDashboardSettings t={t} getCssClasses={getCssClasses} IoSave={IoSave} />
                 </div>
               </Col>
-
             </Row>
-
             {/* Process Screen Settings */}
             <Row className='mt-3'>
               <Col lg={12} md={12} className={``}>
@@ -93,7 +85,6 @@ const UserSettings = () => {
                 <ProcessScreenSettings t={t} getCssClasses={getCssClasses} IoSave={IoSave} />
               </Col>
             </Row>
-
             {/* Security Settings */}
             <Row className='mt-3'>
               <Col lg={12} md={12}>
